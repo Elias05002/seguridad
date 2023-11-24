39db7577c49ec14dd686b3be6a2dd2d55e53a281
Elias05002 <108083138+Elias05002@users.noreply.github.com>
2023-11-23 19:41:30 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var bandera = false;
+
+function validar(){
+    alert($("#user").val());
+    var pass = document.getElementById("password").value;
+    alert(pass)
+    if (bandera){
+        console.log("Validacion completa");
+        //redireccionar a la pagina correspondiente
+    } 
+}
+
+//function validar(){
+   // alert($("#user").val());
+    //alert($("#password").val());
+//$("#user").val("cadena");
+
+//}
+
+
+
+function correctCaptcha(){
+    console.log("Captcha resuelto correctamente");
+    bandera = true;
+}
