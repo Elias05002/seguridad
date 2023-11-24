3a5a9131bc2db82d77b3599cd06aede83dfafb7b
Elias05002 <108083138+Elias05002@users.noreply.github.com>
2023-11-24 00:49:23 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 var bandera = false;
 
 function validar(){
-   // var bandera = document.getElementById("user");
-    //alert($("#user").val());
-  //  var pass = document.getElementById("password").value;
+   // alert($("#user").val());
 
-   // alert(pass)
-    
     if (bandera){
-        console.log("Validacion completa");
-      //  bandera = true;
-        //redireccionar a la pagina correspondiente
-    } 
+        console.log("validación completa");
+        //Redireccione a la pagina correspondiente
+    }
+    
+
+   // var pass = document.getElementById("password");
+   // alert(user.value)
 }
-
-function validar(){
-   alert($("#user").val());
-    alert($("#password").val());
-$("#user").val("cadena");
-
-}
-
-
 
 function correctCaptcha(){
-    console.log("Captcha resuelto correctamente");
-    bandera = true;
+    console.log("Captcha resuelto correctamente")
+    bandera = true;
 }
